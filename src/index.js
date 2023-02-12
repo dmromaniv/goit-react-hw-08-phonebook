b22bbe8cb028f05691da2b1de90f8bd9eef8eb84
Romaniv Dmytro <romanivdmytro@gmail.com>
2023-02-12 12:37:11 +0200

    update basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+  <BrowserRouter basename="/goit-react-hw-08-phonebook">
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
